5e9e77fcbd7dfbe816e3b267ec31855f6165e4cc
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-10-13 21:33:27 +0700

    Update danhsach.js

--- Week5/danhsach.js
+++ Week5/danhsach.js
@@ -1,12 +1,13 @@
 function click_row(self){
 	if (self.checked == true) {
 		self.parentNode.parentNode.classList.add("checked");
+		document.querySelector("div.group-op").classList.remove("nodisplay");
 		console.log(self)
 	}
 	else{
 		self.parentNode.parentNode.classList.remove("checked");
 	}
-}
+};
 // function checkAll(self){
 // 	if (self.checked == true) {
 // 		self.parentNode.parentNode.classList.add("checked");
@@ -31,20 +32,29 @@ function click_row(self){
 // 		}
 
 	// }
-	document.getElementById("checkAll").onchange = function(){
-		let check = document.getElementsByName("check");
-		for(i = 0; i < check.length; i++){
-			check[i].checked = this.checked;
-			if (check[i].checked == true) {
-				check[i].parentNode.parentNode.classList.add("checked");
-			}else{
-				check[i].parentNode.parentNode.classList.remove("checked");
-			}
-		}
-		if (this.checked == true) {
-			document.querySelector("div.group-op").classList.remove("nodisplay");
+document.getElementById("checkAll").onchange = function(){
+	let check = document.getElementsByName("check");
+	for(i = 0; i < check.length; i++){
+		check[i].checked = this.checked;
+		if (check[i].checked == true) {
+			check[i].parentNode.parentNode.classList.add("checked");
 		}else{
-			document.querySelector("div.group-op").classList.add("nodisplay");	
+			check[i].parentNode.parentNode.classList.remove("checked");
 		}
 	}
-}
+	if (this.checked == true) {
+		document.querySelector("div.group-op").classList.remove("nodisplay");
+	}else{
+		document.querySelector("div.group-op").classList.add("nodisplay");	
+	}
+};
+
+document.querySelector(".group-op-delete").onchange = function() {
+	let c = document.getElementsByName("check");	
+	console.log(c); 		
+	for (let i = c.length-1; i >= 0; i--)
+		if (c[i].checked) {
+			c[i].parentNode.parentNode.parentNode.removeChild(c[i].parentNode.parentNode);
+		}
+};
+
